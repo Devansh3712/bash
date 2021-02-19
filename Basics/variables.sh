14603e3a69a8309715a93045776e17d037b1ad93
Devansh3712 <devanshamity@gmail.com>
2021-02-19 23:53:23 +0530

    variables

---
+++ Basics/variables.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+name=Devansh
+age=18
+
+echo $name
+echo $age
+
+echo "Name of student is $name and age is $age"
+
+#system defined variables
+
+echo $HOME
+echo $PWD
+echo $BASH
+echo $OSTYPE
+
+#combining string variables
+
+echo $name is $age
