2b481080479d772c851249a839c34238f40ddbb8
Devansh3712 <devanshamity@gmail.com>
2021-02-20 16:42:38 +0530

    elif statements

---
+++ Basics/else_if.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+read num1
+read num2
+
+if [ $num1 -gt $num2 ];
+then
+	echo "$num1 is greater"
+elif [ $num1 -lt $num2 ];
+then
+	echo "$num2 is greater"
+else
+	echo "They are equal"
+fi
